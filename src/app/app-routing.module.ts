ecedeaac553d08b8e57c09060f39fdad5aa025fe
ruvcaltd <53894177+ruvcaltd@users.noreply.github.com>
2020-04-22 18:34:17 +0100

    first

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PageOneComponent } from "./page-one/page-one.component";
+import { PageTwoComponent } from './page-two/page-two.component';
+
+
+const routes: Routes = [
+  { path: 'page1', component: PageOneComponent },
+  { path: 'page2', component: PageTwoComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
